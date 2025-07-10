08fae51e2ef18d9b8e071171460d1670f1dfffbf
Ali <aliahmedtaha7077@gmail.com>
2025-07-10 22:35:08 +0800

    Fixed App and Router Mismatch

--- backend-refactor/app/routers/tp070007_auth.py
+++ backend-refactor/app/routers/tp070007_auth.py
@@ -10,7 +10,7 @@ from uuid import uuid4
 
 router = APIRouter()
 
-@app.post("/register")
+@router.post("/register")
 def register_user(
     username: str = Form(...),
     password: str = Form(...),
